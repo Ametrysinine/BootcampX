f49bb430c88b067a4aaf5f72eab2950d20f08625
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-04-06 19:17:50 -0700

    Complete BootcampX Queries 3

---
+++ 3_queries_group_by/1_assignments_by_day.sql
@@ -0,0 +1,10 @@
+SELECT
+  day,
+  COUNT(id) AS total_assignments
+FROM
+  assignments
+GROUP BY
+  day
+HAVING COUNT(id) >= 10
+ORDER BY
+  day;
---
+++ 3_queries_group_by/4_average_assignment_duration_per_student.sql
@@ -0,0 +1,12 @@
+SELECT
+  students.name AS student,
+  AVG(duration) AS average_assignment_duration
+FROM
+  students
+  JOIN assignment_submissions ON student_id = students.id
+WHERE
+  students.end_date IS NULL
+GROUP BY
+  students.name
+ORDER BY
+  average_assignment_duration DESC;
