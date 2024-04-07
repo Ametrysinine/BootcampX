7ac1d055c5a5d040eb1d4cae709379db1bab61ab
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-04-06 20:35:37 -0700

    Complete BootcampX Queries 4

---
+++ 4_queries/1_assistance_requests_for_teacher.sql
@@ -0,0 +1,10 @@
+SELECT
+  COUNT(assistance_requests.id) AS total_assistances,
+  teachers.name AS name
+FROM
+  teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+WHERE
+  teachers.name = 'Waylon Boehm'
+GROUP BY
+  teachers.name;
---
+++ 4_queries/9_average_cohort_assistance_time.sql
@@ -0,0 +1,14 @@
+SELECT
+  AVG(total_duration) as average_total_duration
+FROM
+  (
+    SELECT
+      cohorts.name AS cohort,
+      SUM(completed_at - started_at) as total_duration
+    FROM
+      assistance_requests
+      JOIN students ON student_id = students.id
+      JOIN cohorts on cohort_id = cohorts.id
+    GROUP BY
+      cohort
+  ) as average_total_duration
